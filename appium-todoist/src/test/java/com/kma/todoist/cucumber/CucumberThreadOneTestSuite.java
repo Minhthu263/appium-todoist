5867dcf8a35e3e9bf553c8ceb4e2a94514967e5e
Minh Thu <taminhthu263@gmail.com>
2023-06-09 10:51:32 +0700

    test11

--- appium-todoist/src/test/java/com/kma/todoist/cucumber/CucumberThreadOneTestSuite.java
+++ appium-todoist/src/test/java/com/kma/todoist/cucumber/CucumberThreadOneTestSuite.java
@@ -1,12 +1,11 @@
 package com.kma.todoist.cucumber;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
         plugin = {"pretty"},
-        tags = "@parallel1 and not @ignore"
+        tags = "@parallel3 and not @ignore"
 )
 public class CucumberThreadOneTestSuite extends AbstractTestNGCucumberTests {
 }
